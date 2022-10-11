94ee919f107e7b284d1899251d4dba28604685d0
juxxon23 <juxxon23@hotmail.com>
2022-10-11 10:04:11 -0500

    Reestructuracion

--- author_keyword.py
+++ author_keyword.py
@@ -1,43 +1,59 @@
 import pandas as pd
 
+"""
+    To-Do
+     - Falta tener en cuenta los duplicados en grupos con varios 
+       integrantes.
+     - Graficar la red.
+     - Comentar la funcion.
+
+    Observaciones:
+     - Un nombre completo (Se puede filtrar si tiene un punto o no), 
+       realmente solo hay 2 nombres duplicados con punto: César Augusto 
+       Marín López y Diana Marcela Rengifo Arias.
+
+"""
 
 def main():
     df = pd.read_excel("BasedeDatos_completa_2018_2021.xlsx")
     columns = df.columns
     authors = df[columns[2]]
     authors_keywords = []
+    AFK = {}
     
     for author in authors:
         filter_data = df.loc[df[columns[2]] == author, [columns[2], columns[9]]]
         authors_keywords.append(filter_data)
     
-    final_indexes = []
     for author in authors_keywords:
-        a = author
-        a_keywords = []
-        row_indexes = []
-        for row in a.index:
-            a_keywords.append(a.loc[row]['Palabras clave'])
-            if len(a_keywords) == 1:
-                final_indexes.append(row)
-            else:
-                row_indexes.append(row)
-                
-        df_copy = df.drop(df.index[row_indexes])
-        keyword = a_keywords[0].split(',')
-        for word in a_keywords:
-            word = word.split(',')
-            if word != keyword:
-                for w in word:
-                    if w not in keyword:
-                        keyword.append(w)
-        
-        keyword = ','.join(keyword)
-        df_copy.loc[final_indexes[-1],['Palabras clave']] = keyword # NO ASIGNA!!!
-        
-    # Un nombre completo (Se puede filtrar si tiene un punto o no), realmente solo hay 2 nombres duplicados con punto. (César Augusto Marín López. y Diana Marcela Rengifo Arias.)
-    # Falta tener en cuenta las copias de los nombres que estan en grupos.
+        keywords = []
+        for row in author.index:
+            keywords.append(author.loc[row]['Palabras clave'])
+            AFK[author.loc[row]['Investigador Responsable y/o principal']] = keywords
     
+    for author in AFK:
+        if len(AFK[author]) > 1:
+            keyword = AFK[author][0].split(',')
+            for key in AFK[author]:
+                key = key.split(',')
+                if key != keyword:
+                    for k in key:
+                        if k not in keyword:
+                            keyword.append(k)
+            keyword = ','.join(keyword)
+            AFK[author] = [keyword]
+
+    authors_z = authors.values.tolist()
+    keywords_z = []
+    for author in AFK:
+        keywords_z.append(AFK[author])
+
+    zipped = list(zip(authors_z, keywords_z))
+
+    rr = pd.DataFrame(zipped, columns=['Investigador Responsable y/o principal', 'Palabras clave'])
+    
+    return rr
+
 
 if __name__ == "__main__":
     main()
